8f7c07b23c071fcb7d45692f39b596188e2021c6
Julio Freitas <julio.clf31@gmail.com>
2019-09-06 22:12:15 -0300

    build

--- inc/db.js
+++ inc/db.js
@@ -2,7 +2,7 @@ const mysql2 = require('mysql2');
  
 // create the connection to database
 const connection = mysql2.createConnection({
-  host: 'localhost:3306',
+  host: '127.0.0.1',
   user: 'jotaw647',
   database: 'jotaw647_provadev',
   password: 'julioefran261289',
